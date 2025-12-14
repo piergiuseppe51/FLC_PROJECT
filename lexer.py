9a190340e5a3d39d2a868d4b30bcabebd63ed8b8
AndreaP24 <paciandrea24@gmail.com>
2025-12-14 17:50:32 +0100

    Aggiunte le regole complesse per l'identificazione di variabili, interi e stringhe. Aggiunte regole di supporto per newline, gestione di caratteri non riconosciuti e caratteri da ignorare.

--- lexer.py
+++ lexer.py
@@ -52,8 +52,8 @@ tokens = [
 # -----------------------------------------
 
 # ---------------Simple Rules--------------
-
 # Prefix t_ + Token_name = r'Regex'
+
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
@@ -75,4 +75,38 @@ t_COMMA = r','
 # -----------------------------------------
 
 # --------------Complex Rules--------------
+# Prefix t_ + Token_name(token): r'Regex' and operations
 
+# Identify variable names
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+# Identify integer numbers
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Identify strings
+def t_STRING(t):
+    r'(\"([^\\\n]|(\\.))*?\")|(\'([^\\\n]|(\\.))*?\')'
+    t.value = t.value[1:-1]
+    return t
+
+# Identify new lines
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    t.type = 'NEWLINE'
+    return t
+
+# Identify unrecognised characters and skip them
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}' in line {t.lexer.lineno}")
+    t.lexer.skip(1)
+
+# Characters to ignore
+t_ignore = ' '
+t_ignore_COMMENT = r'\#.*'
